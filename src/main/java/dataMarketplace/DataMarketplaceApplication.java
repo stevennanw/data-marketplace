0feda3483054f0e7d498370020781c357f79062c
stevennanw <nan.wu1009@gmail.com>
2022-03-03 22:27:39 +0000

    initialise spring boot

---
+++ src/main/java/dataMarketplace/DataMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package dataMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dataMarketplace/DataMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package dataMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
