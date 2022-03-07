40b273da9e1d3b4b0ed1cc57699b7348b09919ea
YimingZhao6324 <yimingzhao502@gmail.com>
2022-03-07 15:17:49 +0000

    Sign up funcation added

--- src/main/java/dataMarketplace/LoginSignupController.java
+++ src/main/java/dataMarketplace/LoginSignupController.java
@@ -1,11 +1,19 @@
 package dataMarketplace;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
 
 @Controller
-public class LoginSignupController {
-
+public class LoginSignupController implements Serializable {
+    HashMap<Integer, Owner> myLocation = new HashMap<>();
+    int count = 0;
     @GetMapping("/login-owner")
     public String loginOwner() {
         return "login-owner.html";
@@ -21,8 +29,21 @@ public class LoginSignupController {
         return "signup-owner.html";
     }
 
+    @PostMapping("/signup-owner")
+    public void addOwner(Owner data, HttpServletResponse response) {
+        data.setOwnerId(count);
+        myLocation.put(count, data);
+        count++;
+        try {
+            response.sendRedirect("/owner-index");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     @GetMapping("/signup-customer")
     public String signupCustomer() {
         return "signup-customer.html";
     }
+
 }
---
+++ src/main/java/dataMarketplace/Owner.java
@@ -0,0 +1,34 @@
+package dataMarketplace;
+
+public class Owner {
+
+    private String password;
+    private String email;
+    private int ownerId;
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+}
