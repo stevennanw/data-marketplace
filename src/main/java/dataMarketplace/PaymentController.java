3a330a65e9c9a27392dea765e65355b31292a327
stevennanw <nan.wu1009@gmail.com>
2022-03-15 18:12:20 +0000

    cart update

---
+++ src/main/java/dataMarketplace/PaymentController.java
@@ -0,0 +1,12 @@
+package dataMarketplace;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaymentController {
+    @GetMapping("/payment")
+    public String toPayment() {
+        return "payment.html";
+    }
+}
