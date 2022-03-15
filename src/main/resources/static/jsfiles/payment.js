b444d4a6cb2425eb736d70cc85440b99bc1e53c5
pangbai8 <pangbai8@gmail.com>
2022-03-15 19:19:49 +0000

    debugging

--- src/main/resources/static/jsfiles/payment.js
+++ src/main/resources/static/jsfiles/payment.js
@@ -1,8 +1,18 @@
 var removeCartItemButton = document.getElementsByClassName('remove-btn')
+console.log(removeCartItemButton)
 for(var i=0;i<removeCartItemButton.length;i++){
-    var button = removeCartItemButton[i]
+    var button = removeCartItemButton[0]
+    console.log(button)
     button.addEventListener('click', function(event){
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.parentElement.remove()
+        updateTotal()
     })
+}
+
+function updateTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRow = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    console.log(cartRow)
 }
