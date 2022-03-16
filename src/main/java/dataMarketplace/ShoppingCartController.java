ea34e455b2bbee1e2729f5dfb4233db7dc17b792
stevennanw <nan.wu1009@gmail.com>
2022-03-16 00:34:29 +0000

    add cart

--- src/main/java/dataMarketplace/ShoppingCartController.java
+++ src/main/java/dataMarketplace/ShoppingCartController.java
@@ -12,26 +12,5 @@ import java.util.HashMap;
 
 @Controller
 public class ShoppingCartController extends HttpServlet {
-    HashMap<Dataset, Integer> shoppingCart = new HashMap<>();
-    @PostMapping("/addcart")
-    public void addcart(Dataset dataset1, HttpServletResponse response) {
-        System.out.println("Hello");
-        if(!shoppingCart.containsKey(dataset1))
-        {
-            shoppingCart.put(dataset1,1);
-        }else{
-            shoppingCart.put(dataset1,shoppingCart.get(dataset1)+1);
-        }
-    }
 
-    @GetMapping("/payment")
-    public String browseCart(Model model) {
-        //setDatasets(shoppingCart);
-        System.out.println("Hi");
-        for (int i =0; i<shoppingCart.size();i++){
-            System.out.println(shoppingCart.values());
-        }
-        model.addAttribute("datasets", shoppingCart.values());
-        return "payment.html";
-    }
 }
